8418d94b3cb967125e9c54d4e2dffe41b4266b29
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2020-01-19 10:03:43 +0100

    Adds cache on ready event log

--- src/cache/index.js
+++ src/cache/index.js
@@ -7,9 +7,17 @@ const client = redis.createClient();
 const get = promisify(client.get).bind(client);
 
 client.on("error", onError);
+client.on("ready", onReady);
 
 function onError(error) {
   logger.log(error, logger.LEVELS.ERROR);
+}
+
+function onReady(error) {
+  logger.log(
+    "Cache connection has been established successfully.",
+    logger.LEVELS.INFO
+  );
 }
 
 async function set(key, value) {
